1d6fa41dd21abffd8f680399434bc8425d202f90
Sven <svenmijic@gmail.com>
2020-02-11 15:49:40 +0100

    End of Section 15

--- client-app/src/app/common/util/util.ts
+++ client-app/src/app/common/util/util.ts
@@ -1,8 +1,31 @@
+import { IUser } from "../../models/user";
+import { IActivity, IAttendee } from "../../models/activity";
+
 export const combineDateAndTime = (date: Date, time: Date) => {
-    const timeString = time.getHours() + ":" + time.getMinutes() + ":00";
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const dateString = `${year}-${month}-${day}`;
-    return new Date(dateString + " " + timeString);
-}
+  const timeString = time.getHours() + ":" + time.getMinutes() + ":00";
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const dateString = `${year}-${month}-${day}`;
+  return new Date(dateString + " " + timeString);
+};
+
+export const setActivityProps = (activity: IActivity, user: IUser) => {
+  activity.date = new Date(activity.date);
+  activity.isGoing = activity.userActivities.some(
+    a => a.username === user.username
+  );
+  activity.isHost = activity.userActivities.some(
+    a => a.username === user.username && a.isHost
+  );
+  return activity;
+};
+
+export const createAttendee = (user: IUser): IAttendee => {
+  return {
+    displayName: user.displayName,
+    isHost: false,
+    username: user.username,
+    image: user.image!
+  };
+};
