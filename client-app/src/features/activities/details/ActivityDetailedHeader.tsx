1275cb6d093f967e465441f8787cfaf0f486644c
Sven <svenmijic@gmail.com>
2020-02-18 13:12:58 +0100

    End of Section 17

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -24,6 +24,7 @@ const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({
 }) => {
   const rootStore = useContext(RootStoreContext);
   const { attendActivity, cancelAttendance, loading } = rootStore.activityStore;
+  const host = activity.userActivities.filter(x => x.isHost)[0];
   return (
     <Segment.Group>
       <Segment basic attached="top" style={{ padding: "0" }}>
@@ -41,10 +42,13 @@ const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({
                   content={activity.title}
                   style={{ color: "white" }}
                 />
-
                 <p>{format(activity.date, "eeee do MMMM")}</p>
                 <p>
-                  Hosted by <strong>Bob</strong>
+                  Hosted by{" "}
+                  <Link to={`/profile/${host.username}`}>
+                    {" "}
+                    <strong>{host.displayName}</strong>
+                  </Link>
                 </p>
               </Item.Content>
             </Item>
